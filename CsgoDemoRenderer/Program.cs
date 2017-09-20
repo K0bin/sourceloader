89171d8eb814dacaf0492779ff78b92fdc5621ef
Robin <robin.kertels@outlook.com>
2017-09-20 14:19:38 +0200

    Projektdateien hinzufügen.

---
+++ CsgoDemoRenderer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsgoDemoRenderer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
